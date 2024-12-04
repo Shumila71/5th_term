884ea42291ff7c14261eca172c26a263babd6809
Mark <noise-04@mail.ru>
2024-12-04 14:35:37 +0300

    aksp kursach

---
+++ АКСП/курсач/afisha_shop/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO event (name, available_tickets) VALUES ('Concert', 100);
+INSERT INTO event (name, available_tickets) VALUES ('Football Match', 50);
+INSERT INTO event (name, available_tickets) VALUES ('Basketball Match', 350);
+1.	2.	3.	4.	Postman [Электронный ресурс]. – URL: https://www.postman.com/ (дата обращения 15.10.2024). 5. Яндекс афиша 6. Ticketland 7. МТС live
